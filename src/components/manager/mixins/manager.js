e8c4eb6dfc9d7f8c8ed045f504b0964c03157b31
jakimoff <jakimoff1990@mail.ru>
2021-03-30 12:30:18 +0300

    feat: add multi select for MacOS

--- src/components/manager/mixins/manager.js
+++ src/components/manager/mixins/manager.js
@@ -111,7 +111,9 @@ export default {
       }
 
       // single select
-      if ((!event.ctrlKey && !alreadySelected) || (!event.metaKey && !alreadySelected)) this.$store.commit(`fm/${this.manager}/changeSelected`, { type, path });
+      if (!event.ctrlKey && !alreadySelected || !event.metaKey && !alreadySelected) {
+        this.$store.commit(`fm/${this.manager}/changeSelected`, { type, path });
+      }
     },
 
     /**
